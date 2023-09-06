44bd2692a2a16b3f0ca83180982ff42e7f2c01c8
AbbasAwad <137558690+AbbasAwad@users.noreply.github.com>
2023-09-06 10:48:18 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Loge_Screen1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblClock = new System.Windows.Forms.Label();
+            this.lblSup = new System.Windows.Forms.Label();
+            this.lblMessageError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Awkward", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(198, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 49);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOGE IN";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel1.Location = new System.Drawing.Point(72, 367);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 1);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(214)))));
+            this.panel3.Location = new System.Drawing.Point(72, 455);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(450, 1);
+            this.panel3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(217)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Awkward", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(57, 527);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(465, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "LOGE IN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Loge_Screen1.Properties.Resources.unlock;
+            this.pictureBox4.Location = new System.Drawing.Point(57, 411);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Loge_Screen1.Properties.Resources.download;
+            this.pictureBox2.Location = new System.Drawing.Point(57, 323);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Loge_Screen1.Properties.Resources.Globe_Save_World_People_Care_Hands_Taking_Care_People_Save_Protect_World_Earth_Care_Logo_Icon_Element_Vector_On_White_Background_Stock_Vector___Illustration_of_background__community__179233659;
+            this.pictureBox1.Location = new System.Drawing.Point(194, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("PMingLiU-ExtB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(217)))));
+            this.label2.Location = new System.Drawing.Point(394, 488);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Clear Fields";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("PMingLiU-ExtB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(217)))));
+            this.label3.Location = new System.Drawing.Point(263, 614);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Exit";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserName.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(217)))));
+            this.txtUserName.Location = new System.Drawing.Point(103, 337);
+            this.txtUserName.Multiline = true;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(419, 22);
+            this.txtUserName.TabIndex = 1;
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(217)))));
+            this.txtPassword.Location = new System.Drawing.Point(103, 426);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(419, 22);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblDate.Location = new System.Drawing.Point(24, 57);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(46, 20);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "Data";
+            // 
+            // lblClock
+            // 
+            this.lblClock.AutoSize = true;
+            this.lblClock.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClock.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblClock.Location = new System.Drawing.Point(49, 27);
+            this.lblClock.Name = "lblClock";
+            this.lblClock.Size = new System.Drawing.Size(48, 20);
+            this.lblClock.TabIndex = 7;
+            this.lblClock.Text = "Time";
+            // 
+            // lblSup
+            // 
+            this.lblSup.AutoSize = true;
+            this.lblSup.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSup.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblSup.Location = new System.Drawing.Point(31, 25);
+            this.lblSup.Name = "lblSup";
+            this.lblSup.Size = new System.Drawing.Size(22, 20);
+            this.lblSup.TabIndex = 8;
+            this.lblSup.Text = "A";
+            // 
+            // lblMessageError
+            // 
+            this.lblMessageError.AutoSize = true;
+            this.lblMessageError.Font = new System.Drawing.Font("PMingLiU-ExtB", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessageError.ForeColor = System.Drawing.Color.Red;
+            this.lblMessageError.Location = new System.Drawing.Point(31, 656);
+            this.lblMessageError.Name = "lblMessageError";
+            this.lblMessageError.Size = new System.Drawing.Size(0, 18);
+            this.lblMessageError.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(593, 816);
+            this.Controls.Add(this.lblMessageError);
+            this.Controls.Add(this.lblSup);
+            this.Controls.Add(this.lblClock);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblClock;
+        private System.Windows.Forms.Label lblSup;
+        private System.Windows.Forms.Label lblMessageError;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loge_Screen1
+{
+    public partial class Form1 : Form
+    {
+        int loginFailedAttempts = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void _ReastLoginForm()
+        {
+            txtPassword.Clear();
+            txtUserName.Clear();
+            loginFailedAttempts = 0;
+            lblMessageError.ResetText();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtUserName.Text=="abbas" && txtPassword.Text == "123" )
+            {
+                _ReastLoginForm();
+                new Form2().Show();
+                this.Hide();
+            }
+            else
+            {
+                    lblMessageError.Text = "Inavlid User Name Or Password!! \n You have "
+                                        + (2 - loginFailedAttempts) + " attempts before lock your account";
+                loginFailedAttempts++;
+                if(loginFailedAttempts==3)
+                {
+                    button1.Enabled = false;
+                    lblMessageError.Text = "You Are Locked after 3 Faild Trails!!" +
+                                           " \n Contact System Administrators to Unlock Your Account";
+                    return;
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            txtUserName.Clear();
+            txtPassword.Clear();
+            txtUserName.Focus();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtUserName.Focus();
+            CenterToScreen();
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += timerElapsed;
+            timer.Start();
+        }
+        private void timerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            lblClock.Invoke((MethodInvoker)delegate
+            {
+                lblDate.Text = DateTime.Now.ToString("dddd, MMMM yyyy");
+                lblClock.Text = DateTime.Now.ToString("hh:mm:ss");
+                lblSup.Text = DateTime.Now.ToString("tt"); //AM PM
+            });
+        }
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
